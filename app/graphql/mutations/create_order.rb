58c13e612c62824b2ea39f94cdebd0c13fcb7abd
John Santosuosso <john.santosuosso1@gmail.com>
2022-07-23 15:12:15 -0400

    update mutation create order

--- app/graphql/mutations/create_order.rb
+++ app/graphql/mutations/create_order.rb
@@ -1,14 +1,13 @@
 module Mutations
   class CreateOrder < BaseMutation
     argument :description, String, required: true
-    argument :description, Float, required: true
+    argument :total, Float, required: true
 
     field :order, Types::OrderType, null: false
     field :errors, [String], null: false
 
     def resolve(description:, total:)
-      order = Order.new(description: description, total: total,
-      reference_key: SecureRandom.uuid)
+      order = Order.new(description: description, total: total)
 
       if order.save
         {
